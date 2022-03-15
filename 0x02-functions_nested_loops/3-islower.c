ce076b1b55a32f071bdaef927e51f459b7d687a7
bkhalees <sallyatieno96@gmail.com>
2022-03-15 14:43:54 -0700

    My commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase letters
+ *
+ * @c: check character
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	return ((c >= 97) && (c <= 122));
+}
