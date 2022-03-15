985dc27f1db3a463afa42e343149ad9e47768027
bkhalees <sallyatieno96@gmail.com>
2022-03-15 12:13:29 -0700

    My commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: Print a word using _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char a[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(a[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
