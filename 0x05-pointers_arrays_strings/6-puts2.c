09ec97ceb694af4d9b34c12825148274c0b55d4a
bkhalees <sallyatieno96@gmail.com>
2022-03-22 20:50:17 -0700

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+
+/**
+ * puts2 - skip values by 1ine string.
+ *
+ * @str: type char.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 0;
+	char *j = str;
+	int k;
+
+	while (*j != '\0')
+	{
+		j++;
+		len++;
+	}
+	i = len - 1;
+	for (k = 0 ; k <= i ; k++)
+	{
+	if (k % 2 == 0)
+	{
+		_putchar(str[k]);
+	}
+	}
+	_putchar('\n');
+}
