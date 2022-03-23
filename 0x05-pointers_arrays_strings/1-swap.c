f8eee70679119f75546395664d3388a67f37ffd2
bkhalees <sallyatieno96@gmail.com>
2022-03-22 20:06:03 -0700

    Dont swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swap parameter values.
+ *
+ * @a: pointer to an int.
+ * @b: pointer to an int.
+ *
+ * Return: void, nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int h = *a;
+	*a = *b;
+	*b = h;
+
+}
