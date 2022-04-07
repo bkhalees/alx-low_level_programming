51f75874d0563faeb6d4d08946b9e6115655a777
bkhalees <sallyatieno96@gmail.com>
2022-04-07 13:32:24 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@
  * n: s2's number of bytes
  * Return: 0
  */
-char *string_nconcat(char *s1, char *s2)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *arr;
 	unsigned int i, j, co, co_2;
